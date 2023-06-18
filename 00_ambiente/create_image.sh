2effe068c333c781fca0283de5909833f0c24781
Orlewilson Maia <orlewilson.maia@gmail.com>
2023-06-18 16:36:07 -0400

    Adição pk-codigo-exemplo-01 - Criando Imagem Personalizada
    Signed-off-by: Orlewilson Maia <orlewilson.maia@gmail.com>

---
+++ 00_ambiente/create_image.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Criar imagem
+docker build -t ambiente-prog-web-back-end .
+
+# --tag , -t		Name and optionally a tag in the name:tag format
+
+# Fonte: https://docs.docker.com/engine/reference/commandline/build/
